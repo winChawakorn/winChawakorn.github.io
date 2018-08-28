3eec2ad504fd2e6e789a679995a9d5dfe4c759b6
winChawakorn <chawakorn.su@ku.th>
2018-08-28 11:55:24 +0700

    Change Card layout and add more Noburo image

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 import { Icon, Image } from 'semantic-ui-react'
 import Card from './Card'
 import noburo from '../static/noburo.png'
+import noburoall from '../static/noburoall.jpg'
 import localhackday from '../static/localhackday.png'
 import localhackday1st from '../static/localhackday1st.JPG'
+import localhackdayall from '../static/localhackdayall.jpeg'
 
 class Experiences extends Component {
   render() {
@@ -16,13 +18,19 @@ class Experiences extends Component {
             image={noburo}
             title="Noburo"
             detail="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem, aliquid, illum ex nobis eos reiciendis exercitationem provident repellat laudantium saepe veritatis. Distinctio tempore aliquid eaque velit assumenda laudantium ullam magni?"
+            footer={<Image src={noburoall} size="huge" />}
           />
           <Card
             href="https://youtu.be/_n9Djh__SBo"
             image={localhackday}
             title="Local Hack Day"
             detail="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem, aliquid, illum ex nobis eos reiciendis exercitationem provident repellat laudantium saepe veritatis. Distinctio tempore aliquid eaque velit assumenda laudantium ullam magni?"
-            footer={<Image src={localhackday1st} size="huge" />}
+            footer={
+              <span>
+                <Image src={localhackday1st} size="huge" />
+                <Image src={localhackdayall} size="huge" />
+              </span>
+            }
           />
         </div>
       </div >
