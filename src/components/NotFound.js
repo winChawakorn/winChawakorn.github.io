2ee5ad9a81a08c185b176a5f8081120a0fb44d49
winChawakorn <chawakorn.su@ku.th>
2018-09-11 09:48:12 +0700

    Add NotFound page

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NotFound extends Component {
+    render() {
+        return (
+            <div style={{ padding: '40px' }}>
+                404 Not Found
+            </div>
+        )
+    }
+}
